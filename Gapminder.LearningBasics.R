cb022cc19a9c3dd55090d3637c398a863ffbf485
Bella Cornutt <isabellacornutt@icloud.com>
2025-01-23 13:51:36 -0800

    steps 4-8 on the Learning R Basics lab assignment

--- Gapminder.LearningBasics.R
+++ Gapminder.LearningBasics.R
@@ -17,4 +17,19 @@ median(gapminder$lifeExp)
 median(gapminder$gdpPercap)
 
 # two-way table of year and continent ------------------------------
+table(gapminder$year, gapminder$continent)
+table(gapminder$continent, gapminder$year)
+# use the one on the bottom
 
+# making my own data
+name <- c("ross", "rachel", "monica", "joey", "chandler", "phoebe")
+gender <- c("man", "woman", "woman", "man", "man", "woman")
+pets <- c(1, 3, 2, 1, 1, 2)
+likes_skiing <- c(FALSE, TRUE, FALSE, FALSE, TRUE, TRUE)
+
+# my own fake data set ---------------------------------------
+my_df <- data.frame(name = name, gender = gender, 
+                    pets = pets, likes_skiing = likes_skiing)
+
+# summary of my fake data set ------------------------------------
+summary(my_df)
