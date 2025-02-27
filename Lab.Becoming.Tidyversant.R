023d0af8c3e05c2cc338ce87336897bb0a0f9e53
Bella Cornutt <isabellacornutt@icloud.com>
2025-02-26 18:50:23 -0800

    New attempt to turn in Becoming Tidyversant Lab

--- Lab.Becoming.Tidyversant.R
+++ Lab.Becoming.Tidyversant.R
@@ -13,7 +13,7 @@ load("data/earnings.RData")
 
 # Tidy it up -----------------------------------------
 
-# group wages into five dollar groups na dcalculate percent of 
+# group wages into five dollar groups and calculate percent of 
 # respondents in each group
 round(prop.table(table(cut(earnings$wages,
                            breaks=seq(from=0, to=100, by=5),
@@ -24,7 +24,7 @@ round(prop.table(table(cut(earnings$wages,
                                          sep="")))),
       3)*100
 
-# calculate mean wages by gender and then reshapedata wide so male and female 
+# calculate mean wages by gender and then reshape data wide so male and female 
 # wage means are on the same line
 rename(pivot_wider(summarize(group_by(earnings, gender),
                              mean_wages=mean(wages)),
